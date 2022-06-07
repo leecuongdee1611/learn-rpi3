2014dc301fa018ae742e9ef7ccb575a3aafdd915
leecuongdee1611 <leecuong.dee1611@gmail.com>
2022-06-07 17:00:18 +0100

    vchar_driver: create device file

--- device-drivers/char_driver/source/.Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/nn/Yocto-Cuong/learn-rpi3/device-drivers/char_driver/source/Module.symvers := sed 's/\.ko$$/\.o/' /home/nn/Yocto-Cuong/learn-rpi3/device-drivers/char_driver/source/modules.order | scripts/mod/modpost -m -a  -o /home/nn/Yocto-Cuong/learn-rpi3/device-drivers/char_driver/source/Module.symvers -e -i Module.symvers   -T -
--- device-drivers/char_driver/source/vchar_driver.mod
+++
@@ -1,2 +0,0 @@
-/home/nn/Yocto-Cuong/learn-rpi3/device-drivers/char_driver/source/vchar_driver.o
-
